c7c62431c68321f9f227359f836ec211d9a36b62
aleix <al3ixgh@gmail.com>
2021-05-26 18:03:16 +0200

    suma,resta,multiplicacion,division funcinoales

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,5 +1,4 @@
 // ----------------- Variables ------------------------
-//numeros
 var screen = document.getElementById("_calculatorScreen"),
     boton0 = document.getElementById("_btn0"),
     boton1 = document.getElementById("_btn1"),
@@ -11,88 +10,154 @@ var screen = document.getElementById("_calculatorScreen"),
     boton7 = document.getElementById("_btn7"),
     boton8 = document.getElementById("_btn8"),
     boton9 = document.getElementById("_btn9"),
+    borrar = document.getElementById("_clear"),
     //Operadores
-    botonResta = document.getElementById("_btnResta"),
     botonSuma = document.getElementById("_btnPlus"),
+    botonResta = document.getElementById("_btnResta"),
     botonMult = document.getElementById("_btnMult"),
     botonDiv = document.getElementById("_btnDiv"),
     botonRaiz = document.getElementById("_btnRaiz"),
     botonPorc = document.getElementById("_btn%"),
     botonEntreX = document.getElementById("_btn/x"),
     botonIgual = document.getElementById("_btnEqual"),
-    values = [0, 0, 0, 0],
-    i = 0,
-    letraOperacion = '';
+    values = [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    x,
+    y,
+    resultado,
+    letraOperacion = '',
+    i = 0;
 
 // ----------------- Funciones ------------------------
 boton0.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 0;
+    values[i] += '0';
     screen.value += 0;
 });
 
 boton1.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 1;
+    values[i] += '1';
     screen.value += 1;
 });
 
 boton2.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 2;
+    values[i] += '2';
     screen.value += 2;
 });
 
 boton3.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 3;
+    values[i] += '3';
     screen.value += 3;
 });
 
 boton4.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 4;
+    values[i] += '4';
     screen.value += 4;
 });
 
 boton5.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 5;
+    values[i] += '5';
     screen.value += 5;
 });
 
 boton6.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 6;
+    values[i] += '6';
     screen.value += 6;
 });
 
 boton7.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 7;
+    values[i] += '7';
     screen.value += 7;
 });
 
 boton8.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 8;
+    values[i] += '8';
     screen.value += 8;
 });
 
 boton9.addEventListener('click', function (e) {
     e.preventDefault();
-    values[i] += 9;
+    values[i] += '9';
     screen.value += 9;
 });
 
 botonSuma.addEventListener('click', function (e) {
     e.preventDefault();
     i++;
-    letraOperacion = a;
+    screen.value = "";
+    x = parseInt(values[0]);
+    letraOperacion = 'a';
 });
 
-// botonIgual.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     i = 0;
-//     values[1] = parseInt(val)
-// }
+botonResta.addEventListener('click', function (e) {
+    e.preventDefault();
+    i++;
+    screen.value = "";
+    x = parseInt(values[0]);
+    letraOperacion = 'b';
+});
+
+botonMult.addEventListener('click', function (e) {
+    e.preventDefault();
+    i++;
+    screen.value = "";
+    x = parseInt(values[0]);
+    letraOperacion = 'c';
+});
+
+botonDiv.addEventListener('click', function (e) {
+    e.preventDefault();
+    i++;
+    screen.value = "";
+    x = parseInt(values[0]);
+    letraOperacion = 'd';
+});
+
+botonIgual.addEventListener("click", function (e) {
+    e.preventDefault();
+    i = 0;
+    y = parseInt(values[1]);
+
+    /* Operaciones:
+    a: suma
+    b: resta
+    c: multiplicación
+    d: división        
+    */
+
+    switch (letraOperacion) {
+        case 'a':
+            resultado = x + y;
+            break;
+        case 'b':
+            resultado = x - y;
+            break;
+        case 'c':
+            resultado = x * y;
+            break;
+        case 'd':
+            if (y != 0) {
+                resultado = x / y;
+            } else {
+                resultado = "Syntax Error";
+            }
+            break;
+        default:
+            break;
+    }
+
+    screen.value = resultado;
+});
+
+borrar.addEventListener('click', function () {
+    screen.value = "";
+    i = 0;
+    values = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+});
