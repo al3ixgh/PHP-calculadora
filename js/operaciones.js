ed5f0cd5433d14195ff0b4b48af65e14168fa2e4
Ayrton Eli Hernández Casas <76624014+JesusHernandez1995@users.noreply.github.com>
2021-05-26 19:34:14 +0200

    Funciona_v1

--- js/operaciones.js
+++ js/operaciones.js
@@ -1,31 +1,33 @@
 // ----------------- Variables ------------------------
-        var screen = document.getElementById("_calculatorScreen"),
-            boton0 = document.getElementById("_btn0"),
-            boton1 = document.getElementById("_btn1"),
-            boton2 = document.getElementById("_btn2"),
-            boton3 = document.getElementById("_btn3"),
-            boton4 = document.getElementById("_btn4"),
-            boton5 = document.getElementById("_btn5"),
-            boton6 = document.getElementById("_btn6"),
-            boton7 = document.getElementById("_btn7"),
-            boton8 = document.getElementById("_btn8"),
-            boton9 = document.getElementById("_btn9"),
-            borrar = document.getElementById("_clear"),
-            //Operadores
-         botonSuma = document.getElementById("_btnPlus"),
-        botonResta = document.getElementById("_btnResta"),
-         botonMult = document.getElementById("_btnMult"),
-          botonDiv = document.getElementById("_btnDiv"),
-         botonRaiz = document.getElementById("_btnRaiz"),
-         botonPorc = document.getElementById("_btn%"),
-       botonEntreX = document.getElementById("_btn/x"),
-        botonIgual = document.getElementById("_btnEqual"),
-            values = [0,0,0,0,0,0,0,0,0],
-                x,
-                y,
-        resultado,
-    letraOperacion = '',
-                 i = 0;
+            var screen = document.getElementById("_calculatorScreen"),
+                boton0 = document.getElementById("_btn0"),
+                boton1 = document.getElementById("_btn1"),
+                boton2 = document.getElementById("_btn2"),
+                boton3 = document.getElementById("_btn3"),
+                boton4 = document.getElementById("_btn4"),
+                boton5 = document.getElementById("_btn5"),
+                boton6 = document.getElementById("_btn6"),
+                boton7 = document.getElementById("_btn7"),
+                boton8 = document.getElementById("_btn8"),
+                boton9 = document.getElementById("_btn9"),
+                borrar = document.getElementById("_clear"),
+  borrar_ultimo_digito = document.getElementById("_deleteLastDigitbtn"),
+                //Operadores
+            botonSuma = document.getElementById("_btnPlus"),
+            botonResta = document.getElementById("_btnResta"),
+            botonMult = document.getElementById("_btnMult"),
+            botonDiv = document.getElementById("_btnDiv"),
+            botonRaiz = document.getElementById("_btnRaiz"),
+            botonPorc = document.getElementById("_btn%"),
+        botonEntreX = document.getElementById("_btn/x"),
+            botonIgual = document.getElementById("_btnEqual"),
+            botonPunto = document.getElementById("_btnDot"),
+            values = [0,0],
+                    x,
+                    y,
+            resultado,
+        letraOperacion = '',
+                    i = 0;
 
 // ----------------- Funciones ------------------------
 boton0.addEventListener('click', function (e) {
@@ -88,11 +90,17 @@ boton9.addEventListener('click', function (e) {
     screen.value += 9;
 });
 
+botonPunto.addEventListener('click', function (e) {
+    e.preventDefault();
+    values[i] += '.';
+    screen.value += '.';
+});
+
 botonSuma.addEventListener('click', function(e){
     e.preventDefault();
     i++;
     screen.value="";
-    x = parseInt(values[0]);
+    x = parseFloat(values[0]);
     letraOperacion = 'a';
 });
 
@@ -100,7 +108,7 @@ botonResta.addEventListener('click', function(e){
     e.preventDefault();
     i++;
     screen.value="";
-    x = parseInt(values[0]);
+    x = parseFloat(values[0]);
     letraOperacion = 'b';
 });
 
@@ -108,7 +116,7 @@ botonMult.addEventListener('click', function(e){
     e.preventDefault();
     i++;
     screen.value="";
-    x = parseInt(values[0]);
+    x = parseFloat(values[0]);
     letraOperacion = 'c';
 });
 
@@ -116,38 +124,55 @@ botonDiv.addEventListener('click', function(e){
     e.preventDefault();
     i++;
     screen.value="";
-    x = parseInt(values[0]);
+    x = parseFloat(values[0]);
     letraOperacion = 'd';
+});
+
+botonRaiz.addEventListener('click', function(e){
+    e.preventDefault();
+    i=0;
+    screen.value="";
+    x = parseFloat(values[0]);
+    letraOperacion = 'e';
 });
 
 botonIgual.addEventListener("click", function(e){
     e.preventDefault();
     i=0;
-    y = parseInt(values[1]);
+    y = parseFloat(values[1]);
 
     /* Operaciones:
             a: suma
             b: resta
             c: multiplicación
-            d: división        
+            d: división 
+            e: raíz       
     */
 
     switch(letraOperacion){
         case 'a':
-            resultado = x + y;
+            resultado = (x + y);
+            resultado = resultado.toFixed(2);
             break;
         case 'b':
             resultado = x - y;
+            resultado = resultado.toFixed(2);
             break;
         case 'c':
             resultado = x * y;
+            resultado = resultado.toFixed(2);
             break;
         case 'd':
             if(y != 0){
                 resultado = x / y;
+                resultado = resultado.toFixed(2);
             } else {
                 resultado = "Syntax Error";
             }
+            break;
+        case 'e':
+            resultado = Math.sqrt(x);
+            resultado = resultado.toFixed(2);
             break;
         default:
             break;
@@ -159,5 +184,10 @@ botonIgual.addEventListener("click", function(e){
 borrar.addEventListener('click', function(){
     screen.value="";
     i=0;
-    values = [0,0,0,0,0,0,0,0,0];
+    values = [0,0];
+});
+
+borrar_ultimo_digito.addEventListener('click', function(){
+    values[i] = values[i].substring(0, values[i].length - 1);
+    screen.value = values[i];
 });
